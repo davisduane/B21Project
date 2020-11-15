76ab59c9c2b57d300ebda3f49432c341a6084bbe
davisduane <74157384+davisduane@users.noreply.github.com>
2020-11-15 13:50:21 -0500

    Update class1.java

--- src/com/company/class1.java
+++ src/com/company/class1.java
@@ -3,6 +3,6 @@ package com.company;
 public class class1 {
 
     public static void main(String[] args) {
-        System.out.println("Hllo from intelliJ");
+        System.out.println("Hello from intelliJ");
     }
 }
